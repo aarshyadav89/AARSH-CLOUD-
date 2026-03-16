58b9971e56516405cc5838863ad1d244f26b7e5c
AARSH YADAV <aarshyadav1@gmail.com>
2026-03-17 01:51:55 +0530

    Add student dictionary and print details

---
+++ dictionary1.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Aarsh",
+    "age": 20,
+    "course": "BCA"
+}
+
+print("Student Name:", student["name"])
+print("Student Age:", student["age"])
+print("Student Course:", student["course"])
